a29c98c38a03bbc3111902d982aebb2e7e46e86d
Sebastian Urrea <lurreaferia@uniminuto.edu.co>
2019-06-19 21:44:34 -0500

    Scripts XBEE

---
+++ Read Data.py
@@ -0,0 +1,38 @@
+# Created by L.S. Urrea [lurreaferia@uniminuto.edu.co], J. Sanchez-Rojas [jsanchezro8@uniminuto.edu.co], & C.A.Sierra [carlos.andres.sierra.v@gmail.com] on May 2019.
+
+# Collaboration acknowlegments: María Fernanda Chaparro & Fernando Alvarez & Santiago Salazar.
+
+# Copyright (c) 2019 L.S. Urrea, J. Sanchez-Rojas, & C.A.Sierra. Research Group on Athenea. All rights reserved.
+
+#
+
+# This file is part of RaspXbee Project.
+
+#
+
+# RaspXbee Project is free software: you can redistribute it and/or modify it under the terms of the
+
+# GNU General Public License as published by the Free Software Foundation, version 3
+
+import serial
+import time
+ser = serial.Serial(
+    port='COM4',\
+    baudrate=9600,\
+    parity=serial.PARITY_NONE,\
+    stopbits=serial.STOPBITS_ONE,\
+    bytesize=serial.EIGHTBITS,\
+        timeout=1)
+
+print("connected to: " + ser.portstr)
+count=1
+joined_seq =""
+seq = []
+while True:
+    for line in ser.readline():
+        seq.append(chr(line)) #convert from ANSII
+        joined_seq = ''.join(str(v) for v in seq)
+
+    print(joined_seq)
+    joined_seq =""
+ser.close()
---
+++ Send Data.py
@@ -0,0 +1,36 @@
+# Created by L.S. Urrea [lurreaferia@uniminuto.edu.co], J. Sanchez-Rojas [jsanchezro8@uniminuto.edu.co], & C.A.Sierra [carlos.andres.sierra.v@gmail.com] on May 2019.
+
+# Collaboration acknowlegments: María Fernanda Chaparro & Fernando Alvarez & Santiago Salazar.
+
+# Copyright (c) 2019 L.S. Urrea, J. Sanchez-Rojas, & C.A.Sierra. Research Group on Athenea. All rights reserved.
+
+#
+
+# This file is part of RaspXbee Project.
+
+#
+
+# RaspXbee Project is free software: you can redistribute it and/or modify it under the terms of the
+
+# GNU General Public License as published by the Free Software Foundation, version 3
+
+import serial
+import time
+
+ser = serial.Serial(
+    port='COM3',\
+    baudrate=9600,\
+    parity=serial.PARITY_NONE,\
+    stopbits=serial.STOPBITS_ONE,\
+    bytesize=serial.EIGHTBITS,\
+        timeout=0)
+
+
+while True:
+    print("ESCRITOR connected to: " + ser.portstr)
+    print("EScribo dato:")
+    ser.write(mensaje.encode('utf-8'))
+    print("dato escrito")
+    print("====FIN=====")
+    time.sleep(4)
+ser.close()
